2fc7bf655c6cc6be75bbb8e9c448f0db890ec806
barak <barakshaham@gmail.com>
2021-05-22 17:53:59 +0300

    screenshots

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,5 +1,5 @@
 export const SERVER_PREFIX = 'http://dataservice.accuweather.com';
-export const API_KEY = 'OaQ1ZSFi6R92fTQaC8gr6QzqxGGmivdE';
+export const API_KEY = 'TTnM0WRzHRBzc9AExGGZchL9kDtUlL82';
 export const DEFAULT_CITY_KEY = '215793';
-
+//OaQ1ZSFi6R92fTQaC8gr6QzqxGGmivdE
 //WGuCMbbnRGQQfwiLHrFOulNGFUsbDBZi
